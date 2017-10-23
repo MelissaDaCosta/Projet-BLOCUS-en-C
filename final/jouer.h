576b696c3b5bfe2e4bb3e2f5265aa412b5408c9b
melissa <mel-dacosta@hotmail.fr>
2017-10-23 20:49:47 +0200

    final

---
+++ final/jouer.h
@@ -0,0 +1,11 @@
+/* DA COSTA MOCRET*/
+/* fichier jouer.h */
+
+#ifndef JOUER_H
+#define JOUER_H
+
+void jouer(int n, int **tab);
+
+/* fonction qui place les deux joueurs et les deplacent */
+
+#endif /* JOUER_H */
