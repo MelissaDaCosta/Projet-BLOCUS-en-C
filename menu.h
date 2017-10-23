094946ea296f27ae5015484a26040f46b4603aa6
melissa <mel-dacosta@hotmail.fr>
2017-10-23 17:40:48 +0200

    premier projet C

---
+++ menu.h
@@ -0,0 +1,16 @@
+/* fichier menu.h */
+/* DA COSTA MOCRET*/
+#ifndef MENU_H
+#define MENU_H
+
+void grille(int n);
+
+/**********************************************************************/
+void menu(void);
+
+/************************************************************************/
+
+void dessinemenu(void);
+
+
+#endif  /* MENU_H */
